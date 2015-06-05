36edf447a94efbe8f741705c309795b54e023c66
briemarie <thebriemarie@gmail.com>
2015-06-04 18:34:22 -0700

    Experiment with hiding subtabs until tab is clicked

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
   $('.tab').click(function(){
-      $('.tab').removeClass("active");
-      $(this).addClass("active");
+      $('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('.subtab-div').css('display', 'block')
   });
 
 });

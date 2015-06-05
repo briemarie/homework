82d057e0aaf7098e722fdd34c352d5b99316b5b9
briemarie <thebriemarie@gmail.com>
2015-06-04 19:21:44 -0700

    Experiment to see if includes works for checking which tab is clicked

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
   $('.tab').click(function(){
       $('.tab').removeClass('active');
       $(this).addClass('active');
-      $('.subtab-div').css('display', 'block')
+      $('.subtab-div').css('display', 'block');
 
-      console.log(this)
-
+      if (this.includes("dashboard")) {
+        $('.subtab').css('visibility', 'visible');
+      }
   });
 
 });

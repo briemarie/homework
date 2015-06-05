9b3dacba0be48472cb7056b2804827a461d647c7
briemarie <thebriemarie@gmail.com>
2015-06-04 19:54:05 -0700

    Remove strung together elements for better targeting

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
       $('.subtab-div').css('display', 'block');
 
       if (this.innerHTML.indexOf("Dashboard") != -1){
-        $('.subtab.dashboard').css('visibility', 'visible');
+        $('.dashboard').css('visibility', 'visible');
       }
       else if (this.innerHTML.indexOf("Sign In") != -1){
-        $('.subtab.sign-in').css('visibility', 'visible');
+        $('.sign-in').css('visibility', 'visible');
       };
   });
 

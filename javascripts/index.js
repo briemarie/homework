321f4ec32a566a5025f6097359bd552fb1fbb99a
briemarie <thebriemarie@gmail.com>
2015-06-04 19:41:35 -0700

    Experiment successful- sub tabs for dashboard display only when dashboard is clicked, disappear when others are

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
   $('.tab').click(function(){
+      // Undoes previous clicks changes
       $('.tab').removeClass('active');
+      $('.subtab').css('visibility', 'hidden');
+
       $(this).addClass('active');
       $('.subtab-div').css('display', 'block');
-
-      console.log(this)
 
       if (this.innerHTML.indexOf("Dashboard") != -1){
         $('.subtab').css('visibility', 'visible');

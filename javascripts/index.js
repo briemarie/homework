9c315e09ed28e89223ba5ee9e5f17795463831e3
briemarie <thebriemarie@gmail.com>
2015-06-04 20:08:14 -0700

    Change from visibility hidden to display none for proper alignment of visible elements

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
 
   $('.tab').click(function(){
       $('.tab').removeClass('active');
-      $('.subtab-list').css('visibility', 'hidden');
+      $('.subtab-list').css('display', 'none');
 
       $(this).addClass('active');
       $('.subtab-div').css('display', 'block');
 
       if (this.innerHTML.indexOf("Dashboard") != -1){
-        $('#dashboard').css('visibility', 'visible');
+        $('#dashboard').css('display', 'list item');
       }
       else if (this.innerHTML.indexOf("Sign In") != -1){
-        $('#sign-in').css('visibility', 'visible');
+        $('#sign-in').css('display', 'list item');
       };
   });
 

260da32a04514d5f257b738a7e6ed5fccb325ccd
briemarie <thebriemarie@gmail.com>
2015-06-04 19:57:30 -0700

    Change additional class to id

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
       $('.subtab-div').css('display', 'block');
 
       if (this.innerHTML.indexOf("Dashboard") != -1){
-        $('.dashboard').css('visibility', 'visible');
+        $('#dashboard').css('visibility', 'visible');
       }
       else if (this.innerHTML.indexOf("Sign In") != -1){
-        $('.sign-in').css('visibility', 'visible');
+        $('#sign-in').css('visibility', 'visible');
       };
   });
 

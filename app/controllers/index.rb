0bd21c39715580fac32716c4864bffdfdcb6a769
briemarie <thebriemarie@gmail.com>
2015-06-07 13:14:33 -0700

    Start adding nokogiri code for parsing api data

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,16 @@
 get '/' do
+  # API calls
+  programs_api = "https://api.myjson.com/bins/5bdb3"
 
+  doc = Nokogiri::HTML(open(programs_api))
+  @program_data = doc.xpath("")
+
+  pricing_api = "https://api.myjson.com/bins/17oy7"
+
+  doc = Nokogiri::HTML(open(programs_api))
+  @pricing_data = doc.xpath("")
+
+  #Template rendering
   erb :index
 end
 

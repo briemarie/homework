a715d340471f4537a9fae4f4b45526f915f4aa29
briemarie <thebriemarie@gmail.com>
2015-06-07 13:19:42 -0700

    Comment out nokogiri for testing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,15 +2,15 @@ require 'nokogiri'
 
 get '/' do
   # API calls
-  programs_api = "https://api.myjson.com/bins/5bdb3"
+  # programs_api = "https://api.myjson.com/bins/5bdb3"
 
-  doc = Nokogiri::HTML(open(programs_api))
-  @program_data = doc.xpath("")
+  # doc = Nokogiri::HTML(open(programs_api))
+  # @program_data = doc.xpath("")
 
-  pricing_api = "https://api.myjson.com/bins/17oy7"
+  # pricing_api = "https://api.myjson.com/bins/17oy7"
 
-  doc = Nokogiri::HTML(open(programs_api))
-  @pricing_data = doc.xpath("")
+  # doc = Nokogiri::HTML(open(programs_api))
+  # @pricing_data = doc.xpath("")
 
   #Template rendering
   erb :index

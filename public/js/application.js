f491c01f8ee58e1f498b5fb323f7b1dd444e2adb
briemarie <thebriemarie@gmail.com>
2015-06-08 18:05:19 -0700

    Get everything working in firefox by changing innerText to innerHTML, make all jquery consistent with add/removeClass

--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,23 @@
 $(document).ready(function(){
 
-  $('.subtab-div').hide();
-  $('.content').hide();
-  $('.additional-stats').hide();
-
   // Tab functions
 
   $('.tab').click(function(){
       // Undoes changes from possible previous clicks
       $('.tab').removeClass('active');
-      $('.subtab-list').hide();
+      $('.subtab-list').addClass('hidden');
       $('.subtab-link').css('color', '#b2b2b2')
-      $('.content').hide();
+      $('.content').addClass('hidden');
 
       // Creates changes on click
       $(this).addClass('active');
-      $('.subtab-div').show();
+      $('.subtab-div').removeClass('hidden');
 
-      if (this.innerText.indexOf("Dashboard") != -1){
-        $('#dashboard').show();
+      if (this.innerHTML.indexOf("Dashboard") != -1){
+        $('#dashboard').removeClass('hidden');
       }
-      else if (this.innerText.indexOf("Sign In") != -1){
-        $('#sign-in').show();
+      else if (this.innerHTML.indexOf("Sign In") != -1){
+        $('#sign-in').removeClass('hidden');
       };
       // Additional tabs would continue here
   });
@@ -31,14 +27,14 @@ $(document).ready(function(){
   $('.subtab-link').click(function(){
     // Undoes changes from possible previous clicks
     $('.subtab-link').css('color', '#b2b2b2')
-    $('.content').hide();
+    $('.content').addClass('hidden');
 
     // Changes active tab color
     $(this).css('color', '#808080')
 
     // Displays content associated with subtab
-    if (this.innerText.indexOf("Business Overview") != -1){
-        $('#business-overview').show();
+    if (this.innerHTML.indexOf("Business Overview") != -1){
+        $('#business-overview').removeClass('hidden');
       }
       // Additional subtabs would continue here
   });
@@ -60,15 +56,15 @@ $(document).ready(function(){
   $('.more-1').click(function(){
     var id = $(this).data("id")
 
-    $('.additional-stats[data-id='+id+']').show();
-    $(this).css('visibility', 'hidden');
+    $('.additional-stats[data-id='+id+']').removeClass('hidden');
+    $(this).addClass('hidden');
   });
 
   $('.less').click(function(){
     var id = $(this).data("id")
 
-    $('.additional-stats[data-id='+id+']').hide();
-    $('.more-1').css('visibility', 'visible');
+    $('.additional-stats[data-id='+id+']').addClass('hidden');
+    $('.more-1').removeClass('hidden');
   });
 
 });
